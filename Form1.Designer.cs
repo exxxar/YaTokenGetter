ddc6ec9280ca23404abf2b9629ee4a73bb1adc4e
Анонимный Аноним <exxxar@users.noreply.github.com>
2018-03-22 12:53:35 +0300

    добавил автоматическую подгрузку аккаунтов из файла xls в список с последующей возможность выбора этих аккаунтов

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -39,6 +39,9 @@
             this.label5 = new System.Windows.Forms.Label();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label6 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -57,15 +60,16 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(150, 20);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 38);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.Size = new System.Drawing.Size(132, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "Client ID";
+            this.label1.Text = "Client ID (ID приложения)";
             // 
             // label3
             // 
@@ -112,12 +116,13 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(12, 84);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(67, 13);
+            this.label5.Size = new System.Drawing.Size(177, 13);
             this.label5.TabIndex = 10;
-            this.label5.Text = "Client Secret";
+            this.label5.Text = "Client Secret (пароль приложения)";
             // 
             // textBox6
             // 
+            this.textBox6.Enabled = false;
             this.textBox6.Location = new System.Drawing.Point(15, 213);
             this.textBox6.Name = "textBox6";
             this.textBox6.Size = new System.Drawing.Size(371, 20);
@@ -132,11 +137,37 @@
             this.label6.TabIndex = 12;
             this.label6.Text = "access_token";
             // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(405, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(215, 186);
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "*.xls";
+            this.openFileDialog1.FileName = "*.xls";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(405, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(180, 23);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Загрузить аккаунты из файла";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(395, 249);
+            this.ClientSize = new System.Drawing.Size(632, 249);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.label5);
@@ -150,6 +181,7 @@
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Получение тоукена";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,6 +200,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -10,12 +10,32 @@ using System.Windows.Forms;
 using System.IO;
 using System.Net.Mail;
 using System.Net;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Runtime.InteropServices;
 
 namespace RegAccYandex
 {
     public partial class Form1 : Form
     {
+        public class user
+        {
+            public string login { get; set; }
+            public string pass { get; set; }
+            public string prog_id { get; set; }
+            public string prog_pass { get; set; }
 
+            public string toString()
+            {
+                return String.Format("{0};{1};{2};{3}",
+                    this.login,
+                    this.pass,
+                    this.prog_id,
+                    this.prog_pass
+                    );
+            }
+        }
+
+        List<user> users;
         SeleniumModul sm;
 
         public async void SendMail(string smtpServer, string from, string password,
@@ -48,7 +68,8 @@ string mailto, string caption, string message, string attachFile = null)
         {
             InitializeComponent();
             sm = new SeleniumModul();
-           
+            users = new List<user>();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -107,6 +128,92 @@ string mailto, string caption, string message, string attachFile = null)
             textBox1.Text = textBox3.Text = textBox4.Text = textBox5.Text = textBox6.Text = "";
         }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                users.Clear();
+                string filePath = openFileDialog1.FileName;
+                var task = Task.Run(() => this.readExecel(filePath));
+                Task.WaitAll(new Task[] { task });
+                users.ForEach(u => listBox1.Items.Add(u.toString()));
+            }
+            
+        }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+        }
+
+        public async void readExecel(string filePath)
+        {
+            
+            Excel.Application xlApp = new Excel.Application();
+            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(filePath);
+            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+            Excel.Range xlRange = xlWorksheet.UsedRange;
+
+            int rowCount = xlRange.Rows.Count;
+            int colCount = xlRange.Columns.Count;
+
+            if (rowCount < 2)
+                return;
+
+            Console.WriteLine("row=>{0} col=>{0}", rowCount, colCount);
+
+
+            for (int i = 2; i <= rowCount; i++)
+            {
+
+                user u = new user();
+                u.login = ""+xlRange.Cells[i, 3].Value2.ToString();
+                u.pass = "" + xlRange.Cells[i, 4].Value2.ToString();
+                u.prog_id = "" + xlRange.Cells[i, 8].Value2.ToString();
+                u.prog_pass = "" + xlRange.Cells[i, 9].Value2.ToString();
+
+                users.Add(u);
+            }
+
+            //cleanup
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            //rule of thumb for releasing com objects:
+            //  never use two dots, all COM objects must be referenced and released individually
+            //  ex: [somthing].[something].[something] is bad
+
+            //release com objects to fully kill excel process from running in the background
+            Marshal.ReleaseComObject(xlRange);
+            Marshal.ReleaseComObject(xlWorksheet);
+
+            //close and release
+            xlWorkbook.Close();
+            Marshal.ReleaseComObject(xlWorkbook);
+
+            //quit and release
+            xlApp.Quit();
+            Marshal.ReleaseComObject(xlApp);
+        
+
+            
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            user u = users[listBox1.SelectedIndex];
+            textBox3.Text = u.login;
+            textBox4.Text = u.pass;
+            textBox1.Text = u.prog_id;
+            textBox5.Text = u.prog_pass;
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
